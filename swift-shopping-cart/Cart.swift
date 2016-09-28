1cf03708bdcd06f1798e5c82dd11d053148aea77
EnriqueTor <etorrendell@gmail.com>
2016-09-28 00:33:09 -0400

    Complete problems

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -8,5 +8,55 @@
 
 class Cart {
     
+    var items = [Item]()
+    
+    func totalPriceInCents() -> Int {
+        var totalCost = 0
+        for value in items {
+            totalCost += value.priceInCents
+        }
+        return totalCost
+    }
+    
+    func add(item:Item) {
+        items.append(item)
+    }
+
+    func remove(item:Item) {
+        
+        items.remove(at: items.index(of: item)!)
+    }
+
+    func items(withName:String) -> [Item] {
+        var newArray:[Item] = []
+        for i in items {
+            if i.name == withName {
+                newArray.append(i)
+            }
+        }
+        return newArray
+    }
+    
+    func items(withMinPrice:Int) -> [Item] {
+        var newArray:[Item] = []
+        for i in items {
+            if i.priceInCents >= withMinPrice {
+                newArray.append(i)
+            }
+        }
+        return newArray
+    }
+    
+    func items(withMaxPrice:Int) ->[Item] {
+        var newArray:[Item] = []
+        for i in items {
+            if i.priceInCents <= withMaxPrice {
+                newArray.append(i)
+            }
+        }
+        return newArray
+    }
+    
+
     
 }
